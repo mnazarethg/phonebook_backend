7080863a8aacb1fba9f912d484a79a319e5229e6
mnazarethg <mnazarethg@gmail.com>
2024-06-05 10:52:07 +0200

    Solution error for changes in dist folder

--- index.js
+++ index.js
@@ -73,13 +73,6 @@ app.get('/api/persons/:id', (request, response, next) => {
   .catch(error => next(error))
 })
 
-const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => n.id))
-    : 0
-  return maxId + 1
-}
-
 app.post('/api/persons', (request, response) => {
   const body = request.body
   console.log(body)
@@ -100,13 +93,15 @@ if (existingPerson) {
 const person = new Person({
   name: body.name,
   number: body.number || false,
-  //id: generateId(),
 });
 
-person.save().then(savedPerson => {
+person.save()
+  .then(savedPerson => {
   console.log(`Added ${person.name} number ${person.number} to phonebook`)
   response.json(savedPerson);
-});
+  })
+  .catch(error => next(error))
+
 
 persons = persons.concat(person)
 
@@ -122,14 +117,12 @@ Person.findByIdAndDelete(request.params.id)
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+  const {name, number} = request.body
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  }
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    request.params.id, 
+    { name, number }, 
+    { new: true, runValidators: true, context: 'query' })
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
@@ -146,9 +139,10 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } 
-
+    return response.status(400).send({ error: 'malformatted number' })
+  }  else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
   next(error)
 }
 
